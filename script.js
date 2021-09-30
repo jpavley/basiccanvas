d75151973ece067697019daec3300d72e5db898b
John Pavley <john@pavley.com>
2021-09-29 23:37:42 -0400

    basic template complete

--- script.js
+++ script.js
@@ -1,3 +1,138 @@
 window.addEventListener('load', function() {
-    console.log("content loaded!");
+
+    // get and configure the canvas
+    const canvas = document.getElementById('canvas1');
+    canvas.width = 500;
+    canvas.height = 800;
+    const ctx = canvas.getContext('2d');
+
+    class Game {
+        constructor(ctx, width, height) {
+            this.ctx = ctx;
+            this.width = width;
+            this.height = height;
+
+            this.sprites = [];
+
+            this.interval = 1000;
+            this.timer = 0;
+        }
+
+        update(deltaTime) {
+            if (this.timer > this.interval) {
+
+                // remove and add sprites
+                this.sprites = this.sprites.filter(s => !s.deleteMe);
+                this.#createSprite();
+                console.log(this.sprites);
+                this.timer = 0;
+            } else {
+                this.timer += deltaTime;
+            }
+
+            // update sprites
+            this.sprites.forEach(s => s.update(deltaTime));
+        }
+
+        draw() {
+
+            // draw background
+            ctx.globalAlpha = 1.0;
+            ctx.fillStyle = 'green';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            // draw forground
+            this.sprites.forEach(s => s.draw(this.ctx));
+        }
+
+        #createSprite() {
+            this.sprites.push(new Sprite(this));
+            this.sprites.sort(function(a, b) {
+                // higher sprites will be drawn before lower sprites
+                return a.y - b.y;
+            });
+        }
+    }
+
+    class Sprite {
+        constructor(game) {
+            this.game = game;
+            this.deleteMe = false;
+
+            this.alphas = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+                                0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
+            this.frameX = 0;
+            this.frameCount = this.alphas.length;
+
+            this.frameInterval = 300;
+            this.frameTimer = 0;
+
+            this.width = 80;
+            this.height = 70;
+
+            this.x = this.game.width * Math.random();
+            this.y = 0 - this.height;
+            this.vy = Math.random() * 0.1 + 0.1;
+
+        }
+
+        update(deltaTime) {
+            // animate frame
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX < this.frameCount) {
+                    // advance animation frame
+                    this.frameX += 1;
+                } else {
+                    // reset animation frame
+                    this.frameX = 0;
+                }
+                // reset frame timer
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+
+            // remove sprite when out of bounds
+            if (this.y < 0 - this.height * 2) {
+                // remove from game
+                this.deleteMe = true;
+            }
+
+            // move sprite
+            if (this.y < 0 - this.height * 2) {
+                // remove from game
+                this.markedForDeletion = true;
+            }
+
+            this.y += this.vy * deltaTime;
+            if (this.y > this.game.height - this.height) {
+                this.vy *= -1; // transform to negative
+            }
+        }
+
+        draw(ctx) {
+            ctx.fillStyle = 'white';
+            ctx.globalAlpha = this.alphas[this.frameX];
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    const game = new Game(ctx, canvas.width, canvas.height);
+    let lastTime = 1;
+    function animate(timeStamp) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // deltaTime: elapsed time between frames
+        // faster computer, small value for deltaTime
+        // slower computer, larger value for deltaTime
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+
+        game.update(deltaTime);
+        game.draw();
+        //console.log(deltaTime);
+        requestAnimationFrame(animate);
+    }
+
+    animate(0);
 });
